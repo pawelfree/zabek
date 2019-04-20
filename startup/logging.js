bb1bdeb86f727764cb1403e99433049ba456a5fc
Pawel Dudek <pawelfree@wp.pl>
2019-04-21 01:35:57 +0200

    logging 9

--- startup/logging.js
+++ startup/logging.js
@@ -4,8 +4,9 @@ const morgan = require("morgan");
 module.exports = function(app) {
   winston.add(
     new winston.transports.File({
-      level: "info",
+      level: "debug",
       filename: "application.log",
+      handleExceptions: true,
       format: winston.format.combine(
         winston.format.timestamp(),
         winston.format.json()
@@ -17,21 +18,26 @@ module.exports = function(app) {
     winston.add(
       new winston.transports.Console({
         level: "debug",
+        handleExceptions: true,
         format: winston.format.combine(
           winston.format.colorize(),
           winston.format.simple()
         )
       })
     );
+
+    app.use(
+      morgan("dev", {
+        stream: {
+          write: function(message, encoding) {
+            winston.info(message);
+          }
+        }
+      })
+    );
   }
 
-  app.use(
-    morgan("dev", {
-      stream: {
-        write: function(message, encoding) {
-          winston.info(message);
-        }
-      }
-    })
-  );
+  winston.exitOnError = false;
+
+  require("express-async-errors");
 };
