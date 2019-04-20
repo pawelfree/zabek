d958affe7e45ddd52411bed735eea3098fdfe3de
Pawel Dudek <pawelfree@wp.pl>
2019-04-21 01:00:55 +0200

    logging 8

--- startup/routes.js
+++ startup/routes.js
@@ -4,5 +4,9 @@ const error = require("../middleware/error");
 module.exports = function(app) {
   app.use(express.json());
 
+  app.get("/", (req, res, next) => {
+    res.status(200).send("OK");
+  });
+
   app.use(error);
 };
