52eba223338cec581f7f73b157b0d3ec433d305b
Pawel Dudek <pawelfree@wp.pl>
2019-04-20 22:49:03 +0200

    logging 3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("express")();
 
 const winston = require("./startup/logging")(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
